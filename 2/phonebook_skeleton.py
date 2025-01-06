a903361bdfc16d6b368ac0e057f6df9b3758c758
cj <yuyang0231@gmail.com>
2025-01-06 11:38:49 +0000

    lab2: passed

--- 2/phonebook_skeleton.py
+++ 2/phonebook_skeleton.py
@@ -11,30 +11,44 @@ def main():
         exit(1)
 
     elif sys.argv[1] == "new":
-        # YOUR CODE HERE #
+        f = open(PHONEBOOK_ENTRIES, "a")
+        f.write(sys.argv[2] + " " + sys.argv[3] + "\n")
+        exit(0)
 
     elif sys.argv[1] == "list":
-        if not os.path.isfile(PHONEBOOK_ENTRIES) or os.path.getsize(
-                PHONEBOOK_ENTRIES) == 0:
+        if (
+            not os.path.isfile(PHONEBOOK_ENTRIES)
+            or os.path.getsize(PHONEBOOK_ENTRIES) == 0
+        ):
             print("phonebook is empty")
         else:
-            # YOUR CODE HERE #
-    
+            with open(PHONEBOOK_ENTRIES, "r") as f:
+                for line in f.readlines():
+                    print(line, end="")
+        exit(0)
+
     elif sys.argv[1] == "lookup":
-        # YOUR CODE HERE #
+        name = " ".join(sys.argv[2:])
+        with open(PHONEBOOK_ENTRIES, "r") as f:
+            lookup = list(filter(lambda line: name in line, f.readlines()))
+            for line in lookup:
+                print(line, end="")
+        exit(0)
 
     elif sys.argv[1] == "remove":
         name = " ".join(sys.argv[2:])
-        # YOUR CODE HERE #
+        with open(PHONEBOOK_ENTRIES, "r+") as f:
+            lines_to_keep = list(filter(lambda line: name not in line, f.readlines()))
+            f.seek(0)
+            f.truncate()
+            f.writelines(lines_to_keep)
 
     elif sys.argv[1] == "clear":
-        # YOUR CODE HERE #
+        os.remove(PHONEBOOK_ENTRIES)
+        exit(0)
 
     else:
-        name = " ".join(sys.argv[1:])
-        with open(PHONEBOOK_ENTRIES, 'r') as f:
-            lookup = "".join(filter(lambda line: name in line, f.readlines()))
-            # YOUR CODE HERE #
+        print(f"unknown command {sys.argv[1]}, try <new|list|lookup|remove|clear>")
 
 
 if __name__ == "__main__":
--- 2/phonebook_skeleton.sh
+++ 2/phonebook_skeleton.sh
@@ -7,24 +7,35 @@ if [ "$#" -lt 1 ]; then
     exit 1
 
 elif [ "$1" = "new" ]; then
-    # YOUR CODE HERE #
+    touch ./$PHONEBOOK_ENTRIES
+    echo "$2 $3" >> ./$PHONEBOOK_ENTRIES 
+    exit 0
 
 elif [ "$1" = "list" ]; then
     if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
         echo "phonebook is empty"
     else
-        # YOUR CODE HERE #
+        cat ./$PHONEBOOK_ENTRIES
     fi
+    exit 0
 
 elif [ "$1" = "lookup" ]; then
-    # YOUR CODE HERE #
+    cat $PHONEBOOK_ENTRIES | grep "$2"
+    exit 0
 
 elif [ "$1" = "remove" ]; then
-    # YOUR CODE HERE #
+    # sed -i "s/<old>/<new>/g" ./filename
+    echo "remove $2"
+    sed -i -E "/$2 [0-9]{3}-[0-9]{3}-[0-9]{4}/d" "./$PHONEBOOK_ENTRIES"
+
+    # grep -E "Linus Torvalds [0-9]{3}-[0-9]{3}-[0-9]{4}"
+    exit 0
 
 elif [ "$1" = "clear" ]; then
-    # YOUR CODE HERE #
+    rm ./$PHONEBOOK_ENTRIES
+    exit 0
 
 else
-     # YOUR CODE HERE #
+     echo "Undefined Operation, try 'new', 'list', 'lookup', 'remove', 'clear'"
+     exit 1
 fi
